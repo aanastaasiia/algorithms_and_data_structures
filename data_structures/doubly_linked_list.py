4a2e32850a19aaece83b1360fce39242f4b01e11
anastasia <aaabagaaa@outlook.com>
2025-08-12 15:41:27 +0500

    recursion versions added in sorting methods, remove added in singly linked list

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -10,9 +10,11 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
 
     def append(self, data):
         new_node = DoublyNode(data)
+        self.length += 1
         if not self.head:
             self.head = new_node
             self.tail = new_node
@@ -23,6 +25,7 @@ class DoublyLinkedList:
 
     def prepend(self, data):
         new_node = DoublyNode(data)
+        self.length += 1
         if not self.head:
             self.head = new_node
             self.tail = new_node
@@ -38,6 +41,35 @@ class DoublyLinkedList:
             current = current.next
         print("None")
 
+    def remove(self, ind):
+        if not self.head:
+            raise IndexError("Cannot remove from empty list")
+
+        if ind == 0:
+            self.head = self.head.next
+            self.head.next.prev = self.head
+            self.length -= 1
+            return
+
+        if ind == self.length - 1:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.length -= 1
+            return
+
+        prev_node = self.head
+        for _ in range(ind - 1):
+            if not prev_node.next:
+                raise IndexError("Index out of range")
+            prev_node = prev_node.next
+
+        if not prev_node.next:
+            raise IndexError("Index out of range")
+
+        prev_node.next = prev_node.next.next
+        prev_node.next.prev = prev_node
+        self.length -= 1
+
 
 linked_list = DoublyLinkedList()
 linked_list.append("1st node - head")
@@ -45,4 +77,6 @@ linked_list.append("2nd node")
 linked_list.append("3rd node - last")
 linked_list.display()
 linked_list.prepend("New head")
+linked_list.display()
+linked_list.remove(1)
 linked_list.display()
